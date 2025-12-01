19c37b47a4355ba6326d33764a728e3f85643ebe
co-den <iamagugbueikenna@gmail.com>
2025-12-01 23:29:06 +0100

    updated admincontroller

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -9,9 +9,11 @@ router.use(AuthController.protect);
 router.get("/me", UserController.getCurrentUser);
 // Get all users
 router.get("/", UserController.getAllUsers);
+// Get user by ID
+router.get("/:id", UserController.getUserById);
 // Update user profile
 router.put("/:id", UserController.updateUserProfile);
 // Delete user account
 router.delete("/:id", UserController.deleteUserAccount);
 module.exports = router;
-// Get user by ID
+
