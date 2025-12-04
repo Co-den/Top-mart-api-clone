377bf0247c06c8463324e46ed46953d426229171
co-den <iamagugbueikenna@gmail.com>
2025-12-04 16:00:51 +0100

    updated AdminController.js

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -73,7 +73,7 @@ exports.loginAdmin = async (req, res) => {
     const admin = await Admin.findOne({ email });
     if (!admin) return res.status(401).json({ message: "Invalid credentials" });
 
-    const match = await bcrypt.compare(password, admin.passwordHash);
+    const match = await bcrypt.compare(password, admin.password);
     if (!match) return res.status(401).json({ message: "Invalid credentials" });
 
     createSendToken(admin, 200, req, res);
