cafd790a54fac0f8cef9e3cad2b41714ae953847
co-den <iamagugbueikenna@gmail.com>
2025-12-05 23:12:07 +0100

    updated plansController, plansRoute

--- routes/PlanRoute.js
+++ routes/PlanRoute.js
@@ -4,8 +4,8 @@ const planCtrl = require("../controllers/PlansController");
 const AuthController = require("../auth/authController");
 const authAdmin = require("../auth/adminAuthController");
 
-router.post("/", authAdmin.protect, planCtrl.createPlan);
-router.get("/", authAdmin.protect, planCtrl.getPlans);
+router.post("/create", authAdmin.protect, planCtrl.createPlan);
+router.get("/", planCtrl.getPlans);
 router.post("/:planId", AuthController.protect, planCtrl.buyPlan);
 
 module.exports = router;
