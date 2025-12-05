54525745758e174d293d1f8be598d5f34daa160d
co-den <iamagugbueikenna@gmail.com>
2025-12-05 23:14:56 +0100

    updated plansController, plansRoute

--- model/PlanModel.js
+++ model/PlanModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema(
+const planSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     cycleDays: { type: Number, required: true },
@@ -12,4 +12,4 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model("Plan", productSchema);
