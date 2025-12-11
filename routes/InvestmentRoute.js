b7cd2485c3b6f70201ee1027cd6a92cdd4e01a67
co-den <iamagugbueikenna@gmail.com>
2025-12-11 23:40:11 +0100

    updated planscontroller

--- routes/InvestmentRoute.js
+++ routes/InvestmentRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const investmentController = require("../controllers/investmentController");
+const investmentController = require("../controllers/InvestmentController");
 const onlyAdmin = require("../auth/adminAuthController");
 const onlyUser = require("../auth/authController");
 
