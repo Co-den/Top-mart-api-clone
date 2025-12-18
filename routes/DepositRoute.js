8a1a6fa8926c786bf7aca36acd4e5829c7960ea5
co-den <iamagugbueikenna@gmail.com>
2025-12-18 10:01:50 +0100

    updated

--- routes/DepositRoute.js
+++ routes/DepositRoute.js
@@ -2,11 +2,25 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
 const dC = require("../controllers/DepositController");
 const adminAuthser = require("../auth/adminAuthController");
 const userAuth = require("../auth/authController");
 const authController = require("../auth/authController");
+
+//multer configuration for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/proofs/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
 
 // User initiates deposit
 router.post("/initiate", userAuth.protect, dC.initializeDeposit);
